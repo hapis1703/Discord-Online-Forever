b8a7315ff08e5792270826381aa5ec8bba71a720
Ahmad Hafizh <137069009+hapis1703@users.noreply.github.com>
2025-04-28 18:34:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const keep_alive = require('./keep_alive.js')
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
+bot.on("ready", () => {
+  console.log("Bot is online!");
+
+  bot.editStatus("online", {
+    name: "Always here!",
+    type: 0 // "Playing"
+  });
+});
+
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
